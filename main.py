d1c3a28949ed11ba74fbc8fe3d4ffeafa840eed7
Volodymyr Vladykin <vova@pocketguard.com>
2025-09-23 22:52:54 +0300

    Setup + implement GET responses

---
+++ main.py
@@ -0,0 +1,48 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+import mimetypes
+import pathlib
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == "/":
+            self.send_html_file("static/index.html")
+        elif pr_url.path == "/message.html":
+            self.send_html_file("static/message.html")
+        elif pathlib.Path().joinpath("static/" + pr_url.path[1:]).exists():
+            self.send_static()
+        else:
+            self.send_html_file("static/error.html", 404)
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        with open(filename, "rb") as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        with open(f"static{self.path}", "rb") as file:
+            self.wfile.write(file.read())
+
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ("", 3000)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+
+if __name__ == "__main__":
+    run()
